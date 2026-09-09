f8c567515141224ef14a841eff9a1d6564481170
Mig1881 <miguelrubioibor@gmail.com>
2026-09-09 17:26:19 +0200

    Update script to look for CC##99 in testruns foder

--- .github/scripts/mainframe_operations.sh
+++ .github/scripts/mainframe_operations.sh
@@ -9,15 +9,15 @@ for program in NUMBERS EMPPAY DEPTPAY; do
     # Ejecutamos COBOL Check desde la RAÍZ apuntando al .jar dentro de la carpeta
     java -jar cobol-check/bin/cobol-check-0.2.19.jar -p $program
     
-    # Si se ha generado el código con las pruebas inyectadas (CC##99.CBL), lo subimos al Mainframe
-    if [ -f "CC##99.CBL" ]; then
-        echo "Uploading CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
-        zowe zos-files upload file-to-data-set "CC##99.CBL" "${ZOWE_USERNAME}.CBL($program)"
+    # COBOL Check genera el archivo en la carpeta testruns/
+    if [ -f "testruns/CC##99.CBL" ]; then
+        echo "Uploading testruns/CC##99.CBL to ${ZOWE_USERNAME}.CBL($program)"
+        zowe zos-files upload file-to-data-set "testruns/CC##99.CBL" "${ZOWE_USERNAME}.CBL($program)"
     else
-        echo "CC##99.CBL not found for $program"
+        echo "testruns/CC##99.CBL not found for $program"
     fi
 
-    # Si existe el JCL correspondiente, lo subimos también (ahora estamos en la misma carpeta)
+    # Si existe el JCL correspondiente, lo subimos también
     if [ -f "${program}.JCL" ]; then
         echo "Uploading ${program}.JCL to ${ZOWE_USERNAME}.JCL($program)"
         zowe zos-files upload file-to-data-set "${program}.JCL" "${ZOWE_USERNAME}.JCL($program)"
